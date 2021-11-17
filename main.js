432b8f2ccf86eb35b205097136b66825e1e99be9
Rehaan-Kumar <73431008+Rehaan-Kumar@users.noreply.github.com>
2021-11-17 20:29:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function setup() {
+    canvas = createCanvas(300, 300)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.hide()
+
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/0oFOU5AvI/model.json", modelLoaded)
+}
+
+function modelLoaded() {
+    console.log("Model Loaded :) !!!!!!")
+}
+
+function draw() {
+    image(video , 0, 0, 300, 300)
+    classifier.classify(video, gotResult)
+}
+
+function gotResult(error , results) {
+    if(error) {
+        console.error(error)
+    }else{
+        console.log(results)
+        document.getElementById("name").innerHTML = results[0].label
+        percent = results[0].confidence * 100
+        document.getElementById("Accuracy").innerHTML = percent.toFixed(1) + " %"
+    }
+}
